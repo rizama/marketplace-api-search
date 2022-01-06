2ce8e6bea51f110ccd8d5d638f7b7ab65e034e41
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-06 23:25:30 +0700

    feat: add controller home and test

--- src/jaknot/jaknot.controller.ts
+++ src/jaknot/jaknot.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { JaknotService } from './jaknot.service';
 
 @Controller('jaknot')
-export class JaknotController {}
+export class JaknotController {
+    constructor(private readonly jaknotService: JaknotService) {}
+
+    @Get('/')
+    home() {
+        return this.jaknotService.home();
+    }
+
+    @Get('test')
+    test() {
+        return this.jaknotService.test();
+    }
+}
