812db17bace8a15c137da26d95aa117732923157
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-06 23:25:41 +0700

    feat: add service home and test

--- src/jaknot/jaknot.service.ts
+++ src/jaknot/jaknot.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 @Injectable()
-export class JaknotService {}
+export class JaknotService {
+    home() {
+        return 'Welcome to Jakarta Notebook Scraper';
+    }
+
+    async test() {
+        const url =
+            'https://www.jakartanotebook.com/samsung-portable-ssd-t5-1tb-mu-pa1t0b-black';
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+
+        const price = $('.price-final').text();
+
+        const images = $('.detailGallery').find('a');
+
+        const listImages = [];
+        for (const image of images) {
+            listImages.push($(image).find('img').attr('src'));
+        }
+
+        console.log({ price, listImages });
+    }
+}
