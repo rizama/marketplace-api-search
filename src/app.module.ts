17a033336261fd293cb26eba7d14eae2cb286834
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-07 23:32:48 +0700

    implement config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JaknotModule } from './jaknot/jaknot.module';
 
 @Module({
-  imports: [JaknotModule],
+  imports: [JaknotModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
