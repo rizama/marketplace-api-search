1f38fcd0c83aa0379e916fa465801cacddcd2dd5
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-09 00:07:03 +0700

    libs: add helper response api

--- libs/helper/src/helper.service.ts
+++ libs/helper/src/helper.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { SearchResponseInterface } from 'src/jaknot/dto/search.dto';
 
 @Injectable()
-export class HelperService {}
+export class HelperService {
+    static enumValueToArray(enumData) {
+        return Object.values(enumData);
+    }
+
+    static responseApi(status, message, data) {
+        const result: SearchResponseInterface = {};
+
+        result.status = [200, 201].includes(status) ? 'ok' : 'error';
+        result.message = message;
+        result.data = data;
+
+        return result;
+    }
+}
