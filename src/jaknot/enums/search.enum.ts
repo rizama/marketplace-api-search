dd450d8ea36cbef93e655b3b4c4a6c7887316d0c
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-09 13:43:42 +0700

    add enum Filter

--- src/jaknot/enums/search.enum.ts
+++ src/jaknot/enums/search.enum.ts
@@ -6,3 +6,19 @@ export enum BranchCity {
     Medan = 'medan',
     Yogyakarta = 'yogyakarta',
 }
+
+export enum Filter {
+    NoFilter = 0,
+    AvailableStock = 1,
+    OnlineCod = '0yjwK5',
+    Bandung = 'kmeGKL',
+    Jakbar = 'rz5DK2',
+    Jakut = 'GmLpmq',
+    Cikpua = 'ezZ7xm',
+    Tangerang = 'gKBkzB',
+    Semarang = 'My1Lml',
+    SurabayaTimur = 'wygMzX',
+    SurabayaBarat = 'kK9BzR',
+    Medan = '8zqoyb',
+    YOgyakarta = 'My60z8',
+}
