f801c3795e87ea8c028aadeef11032e1c5dde626
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-11 23:13:18 +0700

    add api property swagger model

--- src/jaknot/models/search.models.ts
+++ src/jaknot/models/search.models.ts
@@ -1,10 +1,75 @@
-import { BranchCity } from "../enums/search.enum";
+import { ApiProperty } from '@nestjs/swagger';
+import { BranchCity } from '../enums/search.enum';
 
 export class SearchQueryDto {
+    @ApiProperty({
+        type: String,
+        description: 'Keyword Search',
+        required: false,
+    })
     query: string;
+
+    @ApiProperty({
+        type: String,
+        description: 'Branch in City',
+        required: false,
+        example: 'bandung',
+        enumName: 'Available Branch',
+        enum: [
+            'bandung',
+            'jabodetabek',
+            'surabaya',
+            'yogyakarta',
+            'semarang',
+            'medan',
+        ],
+    })
     branch: BranchCity;
+
+    @ApiProperty({
+        type: Number,
+        description: 'Limit products result',
+        required: false,
+        default: 40,
+        example: 40,
+        enumName: 'Available Limit Products',
+        enum: [40, 60, 90],
+    })
     show: number;
+
+    @ApiProperty({
+        type: String,
+        description: 'Sorting products',
+        required: false,
+        default: 'match',
+        example: 'match',
+        enumName: 'Available Sorting Type',
+        enum: ['name', 'match', 'lowprice', 'highprice', 'newitem'],
+    })
     sort: string;
+
+    @ApiProperty({
+        type: String,
+        description: 'Show only Available Stock by Branch',
+        required: false,
+        example: 'nofilter',
+        enumName: 'Available Filter Stock',
+        enum: [
+            'nofilter',
+            'availablestock',
+            'onlinecod',
+            'bandung',
+            'jakartabarat',
+            'jakartautara',
+            'cikpua',
+            'tangerang',
+            'semarang',
+            'surabayatimur',
+            'surabayabarat',
+            'medan',
+            'yogyakarta',
+        ],
+    })
     ready: string;
 }
 
