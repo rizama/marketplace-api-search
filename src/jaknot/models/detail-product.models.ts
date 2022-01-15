16f2b1ac667065a31f0fad2aba4bc8b2927d5193
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-15 10:16:45 +0700

    add dto and interface detail product

--- src/jaknot/models/detail-product.models.ts
+++ src/jaknot/models/detail-product.models.ts
@@ -1,4 +1,5 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { branch } from './search.models';
 
 export class DetailParamDto {
     @ApiProperty({
@@ -7,4 +8,34 @@ export class DetailParamDto {
         required: true,
     })
     slug: string;
+}
+
+export class DetailQueryStringDto {
+    branch: string;
+}
+
+export interface DetailResponseInterface {
+    status?: string;
+    message?: string;
+    data?: ResultDetailProduct[];
+}
+
+export interface ResultDetailProduct {
+    detailInfo: detailInfoInterface;
+}
+
+export interface detailInfoInterface {
+    title: string; 
+    rating: number;
+    sku: string;
+    weight: string;
+    warranty: string;
+    colors: color[];
+    branchs: branch[]; 
+}
+
+export interface color {
+    name: string;
+    otherProducts: string;
+    colorCode: string;
 }
