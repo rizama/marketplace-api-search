2d4a6645c866425b3d5c6cd8afb9f386f5d3fdb7
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-25 22:19:38 +0700

    add api property

--- src/shopee/models/search.models.ts
+++ src/shopee/models/search.models.ts
@@ -1,8 +1,45 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class SearchQueryShopeeDto {
+    @ApiProperty({
+        type: String,
+        description: 'Sort Product',
+        required: false,
+    })
     sortBy: string;
+    
+    @ApiProperty({
+        type: String,
+        description: 'Keyword Search',
+        required: false,
+    })
     keyword: string;
+    
+    @ApiProperty({
+        type: Number,
+        description: 'Limit Show Product',
+        required: false,
+    })
     limit: number;
+
+    @ApiProperty({
+        type: Number,
+        description: 'Is Newest or Oldest',
+        required: false,
+    })
     newest: number;
+    
+    @ApiProperty({
+        type: String,
+        description: 'Order',
+        required: false,
+    })
     order: string;
+
+    @ApiProperty({
+        type: String,
+        description: 'Filter by Rating',
+        required: false,
+    })
     ratingFilter: string;
 }
