2c297dc0baa9c7d18c7f9f51fb1cf5d51541f8c1
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-01-29 13:35:37 +0700

    feat: add utils

--- libs/utils/src/common.utils.ts
+++ libs/utils/src/common.utils.ts
@@ -1,17 +1,35 @@
-import { SearchResponseInterface } from "src/jaknot/models/search.models";
-
 export abstract class CommonUtils {
     static enumValueToArray(enumData) {
         return Object.values(enumData);
     }
 
     static responseApi(status, message, data) {
-        const result: SearchResponseInterface = {};
+        const result = {};
 
-        result.status = [200, 201].includes(status) ? 'ok' : 'error';
-        result.message = message;
-        result.data = data;
+        result['status'] = [200, 201].includes(status) ? 'ok' : 'error';
+        result['message'] = message;
+        result['data'] = data;
 
         return result;
+    }
+
+    static verifyValue(value) {
+        return value ?? null;
+    }
+
+    static isArrayAndNotEmpty(values) {
+        if (values && values.length) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static isUndefinedOrNan(value) {
+        if (!value || isNaN(value)) {
+            return true;
+        }
+
+        return false;
     }
 }
