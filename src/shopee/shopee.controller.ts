f87bf1210d9bd63cef6e06a8b8574d678fd8ae8d
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-02-10 22:22:30 +0700

    feat: initial detail endpoint

--- src/shopee/shopee.controller.ts
+++ src/shopee/shopee.controller.ts
@@ -11,24 +11,27 @@ export class ShopeeController {
 
     @Get('v1/search')
     @ApiOkResponse({
-        description: 'Search Endpoint for Jakarta Notebook',
+        description: 'Search Endpoint for Shopee',
     })
-    async search(
-        @Query() queryString: SearchQueryShopeeDto,
-        @Res() response,
-    ){
+    async search(@Query() queryString: SearchQueryShopeeDto, @Res() response) {
         try {
             const seaerchResult = await this.shopeetService.searchShopee(queryString);
-            return response
-                .status(200)
-                .json(CommonUtils.responseApi(200, 'success', seaerchResult));
+            return response.status(200).json(CommonUtils.responseApi(200, 'success', seaerchResult));
         } catch (error) {
-            const message = error.message
-                ? `${error.message}`
-                : `Unexpected Error, ${error}`;
-            return response
-                .status(500)
-                .json(CommonUtils.responseApi('error', message, []));
+            const message = error.message ? `${error.message}` : `Unexpected Error, ${error}`;
+            return response.status(500).json(CommonUtils.responseApi('error', message, []));
+        }
+    }
+
+    @Get('v1/detail')
+    @ApiOkResponse({
+        description: 'Detail Endpoint for Shopee',
+    })
+    async detail(@Res() response) {
+        try {
+        } catch (error) {
+            const message = error.message ? `${error.message}` : `Unexpected Error, ${error}`;
+            return response.status(500).json(CommonUtils.responseApi('error', message, []));
         }
     }
 }
